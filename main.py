12eb36d7e0e2affcdc8a6b299d25ca3e6196250e
Shayan <shayan.delbari@protonmail.com>
2024-02-28 16:02:57 -0400

    adding food and score

--- main.py
+++ main.py
@@ -9,40 +9,87 @@ stdscr.nodelay(True)
 
 obs = '.'
 player = 'x'
+food_ch = '*'
+food_num = 10
+score = 0
 
 maxl = curses.LINES - 1
 maxc = curses.COLS - 1
 
 world = []
+food = []
 
-# player_l = player_c = 0
+# making sure player cannot go outside the screen
+def in_range(a, min, max):
+    if a > max:
+        return max
+    if a < min:
+        return min
+    else:
+        return a
+
+# getting to random values that doesn't overlap
+def random_place():
+    i = random.randint(0, maxl)
+    j = random.randint(0, maxc)
+    while world[i][j] != ' ':
+        i = random.randint(0, maxl)
+        j = random.randint(0, maxc)
+    return i, j
 
 def init():
     global player_c, player_l
-    for i in range(maxl):
+    for i in range(-1, maxl + 1):
         world.append([])
-        for j in range(maxc):
+        for j in range(-1, maxc + 1):
             world[i].append(' ' if random.random() > 0.03 else obs)
-    player_l = random.randint(0, maxl)
-    player_c = random.randint(0, maxc)
+
+    for i in range(food_num):
+        foodl, foodc = random_place()
+        fooda = random.randint(1000, 10000)
+        food.append((foodl, foodc, fooda))
+    
+    player_l, player_c = random_place()
 
 def draw():
     for i in range(maxl):
         for j in range(maxc):
-            stdscr.addch(i, j, world[i][j])
+            stdscr.addch(i, j, world[i][j]) 
+    
+    for f in food:
+        fl, fc, fa = f
+        stdscr.addch(fl, fc, food_ch)
+
     stdscr.addch(player_l, player_c, player)
+    stdscr.addstr(1, 1, f"Score: {score}")
+    
     stdscr.refresh()
 
+# moving function
 def move(c):
     global player_l, player_c
-    if c == 'w':
+    if c == 'w' and world[player_l - 1][player_c] != obs:
         player_l -= 1
-    elif c == 's':
+    elif c == 's' and world[player_l + 1][player_c] != obs:
         player_l += 1
-    elif c == 'a':
+    elif c == 'a' and world[player_l][player_c - 1] != obs:
         player_c -= 1
-    elif c == 'd':
+    elif c == 'd' and world[player_l][player_c + 1] != obs:
         player_c += 1
+
+    player_l = in_range(player_l, 0, maxl - 1)
+    player_c = in_range(player_c, 0, maxc - 1)
+
+# eating food and keeping score
+def check_food():
+    global score
+    for i in range(len(food)):
+        fc, fl, fa = food[i]
+        if player_l == fc and player_c == fl:
+            score += 10
+            new_fl, new_fc = random_place()
+            new_fa = random.randint(1000, 10000)
+            food[i] = (new_fl, new_fc, new_fa)
 
 init()
 
@@ -59,6 +106,8 @@ while playing:
         move(c)
     elif c.lower() == 'q':
         playing = False
+    
+    check_food()
     draw()
 
 curses.endwin()
